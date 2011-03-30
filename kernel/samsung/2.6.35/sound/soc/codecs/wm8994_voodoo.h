2c55d55e241a52bbfe9318b3669549671efcdbe1
Dylan <dylan.taber@gmail.com>
2011-03-29 21:51:50 -0400

    voodoo sound v7

--- kernel/samsung/2.6.35/sound/soc/codecs/wm8994_voodoo.h
+++ kernel/samsung/2.6.35/sound/soc/codecs/wm8994_voodoo.h
@@ -6,12 +6,16 @@
  * published by the Free Software Foundation.
  */
 
-//voodoo sound
+bool is_path(int unified_path);
+
+unsigned int voodoo_hook_wm8994_write(struct snd_soc_codec *codec,
+				      unsigned int reg, unsigned int value);
 void voodoo_hook_fmradio_headset(void);
-unsigned int voodoo_hook_wm8994_write(struct snd_soc_codec *codec, unsigned int reg, unsigned int value);
 void voodoo_hook_wm8994_pcm_probe(struct snd_soc_codec *codec);
 void voodoo_hook_wm8994_pcm_remove(void);
 void voodoo_hook_record_main_mic(void);
+void voodoo_hook_playback_speaker(void);
+
 void update_hpvol(void);
 void update_fm_radio_headset_restore_freqs(bool with_mute);
 void update_fm_radio_headset_normalize_gain(bool with_mute);
@@ -19,15 +23,22 @@ void update_recording_preset(bool with_mute);
 void update_full_bitwidth(bool with_mute);
 void update_osr128(bool with_mute);
 void update_fll_tuning(bool with_mute);
-void update_enable(void);
-unsigned short tune_fll_value(unsigned short val);
-void update_dac_direct(bool with_mute);
 void update_mono_downmix(bool with_mute);
-bool is_path(int unified_path);
+void update_dac_direct(bool with_mute);
+void update_enable(void);
 
-#ifdef CONFIG_SAMSUNG_CAPTIVATE
+unsigned short tune_fll_value(unsigned short val);
+
+
 #define NEXUS_S
-#endif
-//Yeah, it's bad, but if it fixes it...
 
-enum unified_path { HEADPHONES, SPEAKER, MAIN_MICROPHONE };
+
+#ifdef CONFIG_FB_S3C_AMS701KA
+#define GALAXY_TAB
+#endif
+
+#ifdef CONFIG_M110S
+#define M110S
+#endif
+
+enum unified_path { HEADPHONES, RADIO_HEADPHONES, SPEAKER, MAIN_MICROPHONE };

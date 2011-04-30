89ff7695fe81a0d6712550d845fdd366692b4c01
Dylan <dylan.taber@gmail.com>
2011-04-29 23:50:15 -0400

    Revert "Switch to led notification style, now that it's mainline"
    This reverts commit d10a506f37d690084c26017eba2b17c1cea299f8.

--- kernel/samsung/2.6.35/arch/arm/mach-s5pv210/mach-aries.c
+++ kernel/samsung/2.6.35/arch/arm/mach-s5pv210/mach-aries.c
@@ -1166,15 +1166,27 @@ static void touch_keypad_onoff(int onoff)
 	if (onoff == TOUCHKEY_OFF)
 		msleep(30);
 	else
-		msleep(25);
+		msleep(50);
 }
 
-static void touch_keypad_gpio_sleep(int onoff) {
-	if (onoff == TOUCHKEY_ON)
+static void touch_keypad_gpio_onoff(int onoff){
+	if(onoff == TOUCHKEY_ON){
+		/*
+		* reconfigure gpio to activate touchkey controller vdd in sleep mode
+		*/
 		s3c_gpio_slp_cfgpin(_3_GPIO_TOUCH_EN, S3C_GPIO_SLP_OUT1);
-	else
+		s3c_gpio_slp_setpull_updown(_3_GPIO_TOUCH_EN, S3C_GPIO_PULL_NONE);
+	} else {
+		/*
+ 		* reconfigure gpio to deactivate touchkey vdd in sleep mode,
+		* this is the default
+		*/
 		s3c_gpio_slp_cfgpin(_3_GPIO_TOUCH_EN, S3C_GPIO_SLP_OUT0);
+		s3c_gpio_slp_setpull_updown(_3_GPIO_TOUCH_EN, S3C_GPIO_PULL_NONE);
+	}
 }
+
+
 
 static const int touch_keypad_code[] = {
 #if defined (CONFIG_SAMSUNG_GALAXYS) || defined (CONFIG_SAMSUNG_GALAXYSB)
@@ -1194,7 +1206,7 @@ static struct touchkey_platform_data touchkey_data = {
 	.keycode_cnt = ARRAY_SIZE(touch_keypad_code),
 	.keycode = touch_keypad_code,
 	.touchkey_onoff = touch_keypad_onoff,
-	.touchkey_sleep_onoff = touch_keypad_gpio_sleep,
+	.touchkey_sleep_onoff = touch_keypad_gpio_onoff,
 	.fw_name = "cypress-touchkey.bin",
 	.scl_pin = _3_TOUCH_SCL_28V,
 	.sda_pin = _3_TOUCH_SDA_28V,
@@ -2070,7 +2082,7 @@ static struct i2c_board_info i2c_devs10[] __initdata = {
 
 static struct i2c_board_info i2c_devs5[] __initdata = {
 	{
-		I2C_BOARD_INFO("bma023", 0x38),
+		I2C_BOARD_INFO("bma023", (0x38)),
 	},
 };
 
@@ -2236,13 +2248,13 @@ static struct yas529_platform_data yas529_pdata = {
 	.reset_line = GPIO_MSENSE_nRST,
 	.reset_asserted = GPIO_LEVEL_LOW,
 };
+
 static struct i2c_board_info i2c_devs12[] __initdata = {
 	{
 		I2C_BOARD_INFO("yas529", 0x2e),
 		.platform_data = &yas529_pdata,
 	},
 };
-
 
 static struct resource ram_console_resource[] = {
 	{
